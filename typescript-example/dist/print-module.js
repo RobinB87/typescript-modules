8684becf1f4ca6c9329d77571a54ca19ceaf1c36
Robin Bette <robinbette@hotmail.com>
2022-03-16 17:15:18 +0100

    Remove dist folder

--- typescript-example/dist/print-module.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PrintModule = void 0;
-class PrintModule {
-    printMessage(message) {
-        console.log(message);
-    }
-}
-exports.PrintModule = PrintModule;
