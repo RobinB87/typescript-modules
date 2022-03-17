185dc31ae9e9812dee12036b4775602735688926
Robin Bette <robinbette@hotmail.com>
2022-03-17 17:07:22 +0100

    Add test script for logger lib Fix error by injecting the loggerConfig

--- angular-cli-demo/projects/my-console-logger/src/lib/my-console-logger.service.spec.ts
+++ angular-cli-demo/projects/my-console-logger/src/lib/my-console-logger.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MyConsoleLoggerService } from './my-console-logger.service';
+import { LoggerConfig } from './logger.config';
 
 describe('MyConsoleLoggerService', () => {
   let service: MyConsoleLoggerService;
 
+  const loggerConfig: LoggerConfig = {
+    appPrefix: 'anything',
+    isProduction: false,
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: LoggerConfig,
+          useValue: loggerConfig,
+        },
+      ],
+    });
+
     service = TestBed.inject(MyConsoleLoggerService);
   });
 
